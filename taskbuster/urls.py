f9b6229a8d7d0041fa780a1cf05d2de6abb892cd
npouliquen <nick@teampareto.com>
2016-07-19 18:29:07 -0500

    Finished part 4 of tutorial

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from .views import home
+from .views import home, home_files
 
 urlpatterns = [
     # Examples:
@@ -9,5 +9,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', home, name='home')
+    url(r'^$', home, name='home'),
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files, name='home-files'),
 ]
